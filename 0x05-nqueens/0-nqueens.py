db443e854a5d3b46b716682dd1fbd97024a5234d
Medhamza93 <medhamzalahnin@gmail.com>
2024-11-26 11:11:56 -0800

    done

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,59 +1,133 @@
 #!/usr/bin/python3
-"""N Queens"""
-import sys
+"""
+    The N queens game player programming
+"""
+from sys import argv
+from typing import List, Union
 
 
-def print_board(board, n):
-    """Print allocated positions to the queen"""
-    b = []
+class Point:
+    """ A position in the chesse game """
+    def __init__(self, x: int, y: int) -> None:
+        self.xpos = x
+        self.ypos = y
+        self.code = [self.ypos, self.xpos]
 
-    for i in range(n):
-        for j in range(n):
-            if j == board[i]:
-                b.append([i, j])
-    print(b)
+    def horizontal_alignment(self, point) -> bool:
+        """ Check if two poins are horizontally aligned """
+        if self.xpos == point.xpos:
+            return True
+        return False
 
+    def vertical_alignment(self, point) -> bool:
+        """ Check if two poins are vertically aligned """
+        if self.ypos == point.ypos:
+            return True
+        return False
 
-def is_position_safe(board, i, j, r):
-    """Checks if the position is safe for the queen"""
-    return board[i] in (j, j - i + r, i - r + j)
+    def diagonal_alignment(self, point) -> bool:
+        """ Check if two poins are horizontally aligned """
+        if abs(self.xpos - point.xpos) == abs(self.ypos - point.ypos):
+            return True
+        return False
 
+    def __repr__(self) -> str:
+        return f"{self.code}"
 
-def safe_positions(board, row, n):
-    """Find all safe positions where the queen can be allocated"""
-    if row == n:
-        print_board(board, n)
-
-    else:
-        for j in range(n):
-            allowed = True
-            for i in range(row):
-                if is_position_safe(board, i, j, row):
-                    allowed = False
-            if allowed:
-                board[row] = j
-                safe_positions(board, row + 1, n)
-
-
-def create_board(size):
-    """Generates the board"""
-    return [0 * size for i in range(size)]
+    def __str__(self) -> str:
+        return f"[{self.ypos}, {self.xpos}]"
 
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    exit(1)
+class Cheese:
+    """ The chese class """
 
-try:
-    n = int(sys.argv[1])
-except BaseException:
-    print("N must be a number")
-    exit(1)
+    def __init__(self, size=4):
+        """ Initialisation of the chese board """
+        self.N = size
+        self.board = []
+        self.queens = []
+        self.possibilities = []
+        self.failed_paths = []
 
-if (n < 4):
-    print("N must be at least 4")
-    exit(1)
+    def check_queen_insertion_collision(self, point: Point) -> bool:
+        """ Collision checking """
+        # vertical collision
+        for x in self.queens:
+            if point.vertical_alignment(x):
+                return True
+            elif point.horizontal_alignment(x):
+                return True
+            elif point.diagonal_alignment(x):
+                return True
+        return False
 
-board = create_board(int(n))
-row = 0
-safe_positions(board, row, int(n))
+    def back_propagation(self, col: int, row: int) -> Union[bool, None]:
+        """ Analysing each possibility """
+        if col >= self.N or row >= self.N:
+            return True
+        for i in range(col, self.N):
+            point = Point(x=i, y=row)
+            if not self.check_queen_insertion_collision(point=point):
+                self.queens.append(point)
+                self.back_propagation(col=0, row=row+1)
+        if len(self.queens) == self.N:
+            self.possibilities.append(self.queens.copy())
+        else:
+            self.failed_paths.append(self.queens.copy())
+        if len(self.queens) > 0:
+            self.queens.pop()
+
+    def search_n_queens_dispositions(self):
+        """ Searching n queens function parser """
+        for x in range(0, self.N):
+            self.queens = []
+            self.back_propagation(col=x, row=0)
+        if self.N % 2 == 0:
+            self.possibilities.pop(0)
+
+    @staticmethod
+    def print_list_object(list_obj: List):
+        """ Printing the list of the queens """
+        for x in list_obj:
+            print("{}".format(x))
+
+    def __str__(self):
+        """ Stringifying """
+        string = "CHEESE BOARD INFOS\n"
+        string += "\tBoard Size: {}\n".format(self.N)
+        string += "\tACTUAL QUEENS DISPOSITIONS: {}\n".format(len(self.queens))
+        for x in self.queens:
+            string += "\t\t{}".format(x)
+        string += "\n\tN-QUEENS DISPOSITIONS: {}\n".format(
+            len(self.possibilities))
+        for x in self.possibilities:
+            string += "\t\t{}".format(x)
+        string += "\n\tFAILED: {}\n".format(len(self.failed_paths))
+        for x in self.failed_paths:
+            string += "\t\t{}\n".format(x)
+        return string.strip()
+
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    try:
+        N = int(argv[1])
+    except Exception:
+        print("N must be a number")
+        exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    cheese = Cheese(size=N)
+    # print("=====================================")
+    cheese.search_n_queens_dispositions()
+    # print("=====================================")
+    # print("{}".format(cheese))
+    # print("=====================================")
+    # print("{}".format(cheese.failed_paths[:50]))
+    # print("=====================================")
+    cheese.print_list_object(cheese.possibilities)
+    # print("=====================================")
